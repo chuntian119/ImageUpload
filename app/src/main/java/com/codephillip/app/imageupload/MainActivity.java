1a498a3b45b8492121b0d653a4de334fc19998de
codephillip <codephillip@gmail.com>
2017-10-06 02:29:13 +0300

    added send File type to server, added camera

--- app/src/main/java/com/codephillip/app/imageupload/MainActivity.java
+++ app/src/main/java/com/codephillip/app/imageupload/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,6 +14,9 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -27,7 +31,9 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private final String URL = "insert url";
+    private static final int REQUEST_IMAGE_CAPTURE = 3569;
+    private static final int REQUEST_GALLERY_IMAGE = 5884;
+    private final String URL = "https://chapchapafrica.com/attach_file.php";
     private TextView textTargetUri;
     private ImageView targetImage;
 
@@ -39,33 +45,112 @@ public class MainActivity extends AppCompatActivity {
         targetImage = (ImageView)findViewById(R.id.targetimage);
     }
 
+    /**
+     * User uploads image from gallery or takes a picture using the camera
+     * */
     public void uploadImage(View view) {
         Log.d(TAG, "uploadImage: clicked");
-        openGallery();
+//        openGallery();
+        openCamera();
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File convertBitmapToFile(@NonNull Bitmap bitmap) {
+        try {
+            // use getCacheDir to temporarily store the file
+            File file = new File(getCacheDir(), "image.png");
+            file.createNewFile();
+
+            //Convert bitmap to byte array
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+            byte[] bitmapdata = bos.toByteArray();
+
+            //write the bytes in file
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+            return file;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void openGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, 0);
+        startActivityForResult(intent, REQUEST_GALLERY_IMAGE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK){
+        Bitmap bitmap = null;
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            targetImage.setImageBitmap(bitmap);
+        }
+
+        if (requestCode == REQUEST_GALLERY_IMAGE &&resultCode == RESULT_OK){
             try {
                 Uri targetUri = data.getData();
                 textTargetUri.setText(targetUri.toString());
-                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
                 targetImage.setImageBitmap(bitmap);
-                String response = sendImageToServer(targetUri.toString(), "0756878434");
-                Log.d(TAG, "onActivityResult: RESPONSE " + response);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        initialiseImageUpload(bitmap);
+    }
+
+    private void initialiseImageUpload(Bitmap bitmap) {
+        try {
+            if (bitmap != null) {
+                // first convert bitmap because Okhttp only accepts files or URIs
+                File file = convertBitmapToFile(bitmap);
+                String response = sendImageToServer(file, "0756878434");
+                Log.d(TAG, "onActivityResult: RESPONSE " + response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String sendImageToServer(File file, String phoneNumber) throws IOException {
+        Log.d(TAG, "sendImageToServer: started");
+        //todo replace with asynctask
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
+            String imageName = generateImageName();
+            Log.d(TAG, "sendImageToServer: ImageName " + imageName);
+            OkHttpClient client = new OkHttpClient.Builder().connectTimeout(10, TimeUnit.SECONDS).writeTimeout(180, TimeUnit.SECONDS).readTimeout(180, TimeUnit.SECONDS).build();
+            RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                    .addFormDataPart("theFile", imageName, RequestBody.create(MEDIA_TYPE_PNG, file))
+                    .addFormDataPart("phoneNumber", phoneNumber)
+                    .build();
+            Request request = new Request.Builder().url(URL).post(body).build();
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Network failure";
     }
 
     private String sendImageToServer(String fileUri, String phoneNumber) throws IOException {
