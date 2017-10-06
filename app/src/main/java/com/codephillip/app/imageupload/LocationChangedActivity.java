f38653153759c98699f5fc99819dfae345b14bc0
codephillip <codephillip@gmail.com>
2017-10-06 07:06:58 +0300

    added location service and broadcast receiver

---
+++ app/src/main/java/com/codephillip/app/imageupload/LocationChangedActivity.java
@@ -0,0 +1,13 @@
+package com.codephillip.app.imageupload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationChangedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_changed);
+    }
+}
---
+++ app/src/main/java/com/codephillip/app/imageupload/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.codephillip.app.imageupload;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    private final String TAG = MyReceiver.class.getSimpleName();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: STARTED");
+        // broadcast received, indicating that the SetUpService has finished
+        Intent mainIntent = new Intent(context, LocationChangedActivity.class);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(mainIntent);
+    }
+}
